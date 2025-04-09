c6a56941e657b434dbb7225b90304dc0b624c423
Cuongdola <145923275+doancuong357@users.noreply.github.com>
2025-04-09 10:21:20 +0700

    Add src/App.vue 1744168880094

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
